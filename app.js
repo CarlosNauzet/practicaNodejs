4a8880aa7c3bae62ea9b9b00ca1e55a551844531
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-09-30 11:50:25 +0200

    hago una test-route con exito

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+// crear nuestra primera ruta-test
+app.get("/api/v1/test", (req, res) => {
+  console.log(req);
+  res.send("test-route");
+});
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
