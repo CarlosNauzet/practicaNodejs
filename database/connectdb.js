3ef48e276f3008a70945fd77fe9a453de9fa1c5b
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-09-30 11:26:34 +0200

    Solucionado problema conexión BBDD

--- database/connectdb.js
+++ database/connectdb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbconnect = async function connectdb() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/nodepop");
+    await mongoose.connect("mongodb://127.0.0.1:27017/nodepop"); // reemplazo el localhost por el 127.0.0.1 y funciona!
     console.log("BBDD conectada con éxito");
   } catch (error) {
     console.log(error);
