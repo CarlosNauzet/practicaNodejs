0ef182cef49e65f3458166f27df8df9ac4b0890b
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-10-05 19:03:04 +0200

    Arreglos de CSS: flex-wrap

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
-const Productos = require("../models/Product");
+const axios = require("axios");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  const productos = await Productos.find();
-  console.log(productos);
+  const { data } = await axios.get("http://127.0.0.1:3000/api/v1/productos");
+  console.log(data);
+  const productos = data;
   res.render("index", { title: "Express", productos });
 });
 
