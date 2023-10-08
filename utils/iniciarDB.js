a2a1f102aaadc9fed9336055062433b4ac011ca3
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-10-08 11:37:57 +0200

    añado script para poblar la BBDD

---
+++ utils/iniciarDB.js
@@ -0,0 +1,17 @@
+const Productos = require("../models/Product");
+const mongoose = require("mongoose");
+const productos = require("./products");
+const dbconnect = async function connectdb() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/nodepop"); // reemplazo el localhost por el 127.0.0.1 y funciona!
+    console.log("BBDD conectada con éxito");
+    await Productos.deleteMany();
+    console.log("Productos anteriores eliminados");
+    await Productos.insertMany(productos);
+    console.log("Productos insertados");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+dbconnect();
