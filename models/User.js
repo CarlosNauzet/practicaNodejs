0a8c8fd1c601c802eb732b3e385b5441f9b767bc
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-12-14 11:23:19 +0100

    Creo el modelo Usario

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "El campo email es obligatorio"],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const Usuario = mongoose.model("Usuario", UserSchema);
+
+module.exports = Usuario;
