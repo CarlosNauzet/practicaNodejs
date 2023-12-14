946424641c012472ee9a6b7df08c0ef2c460fec5
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-12-14 11:16:13 +0100

    Completo la internacionalización

--- controllers/changeLocaleController.js
+++ controllers/changeLocaleController.js
@@ -1,6 +1,10 @@
 const changeLocale = (req, res) => {
-  console.log(req.params);
-  res.json(req.params);
+  const { locale } = req.params;
+  console.log(locale);
+  res.cookie("nodepop-locale", locale, {
+    maxAge: 1000 * 60 * 60 * 24 * 30, // un mes - 30 días
+  });
+  res.redirect(req.get("referer"));
 };
 
 module.exports = changeLocale;
